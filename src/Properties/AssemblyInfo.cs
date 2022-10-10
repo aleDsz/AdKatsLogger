43da710b51255d7f2a19620399c7648e36d4f820
Alexandre de Souza <alexandre@aledsz.com.br>
2022-10-10 19:50:53 -0300

    Fix assembly name

--- src/Properties/AssemblyInfo.cs
+++ src/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("CChatGUIDStatsLogger")]
+[assembly: AssemblyTitle("AdKatsLogger")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("CChatGUIDStatsLogger")]
+[assembly: AssemblyProduct("AdKatsLogger")]
 [assembly: AssemblyCopyright("Copyright ©  2022")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
